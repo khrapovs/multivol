fa3385d08e239298238817e5555089ecd539b0a0
Stanislav Khrapov <khrapovs@gmail.com>
2016-01-12 16:52:48 +0300

    Clean up

--- examples/examples.py
+++ examples/examples.py
@@ -18,22 +18,6 @@ if __name__ == '__main__':
     np.set_printoptions(precision=4, suppress=True)
     sns.set()
 
-#    persistence = .99
-#    beta = persistence
-#    gamma = .1
-#    alpha = persistence - beta
-#    omega = 0
-#    eta, lam = 30, .0
-#    nobs = 2000
-#
-#    model = arch_model(None, p=1, o=1, q=1, mean='Zero',
-#                       vol='EGARCH', dist='Normal')
-#
-#    data = model.simulate([omega, alpha, gamma, beta], nobs=nobs)
-#
-#    data[['data', 'volatility']].plot()
-#    plt.show()
-
     nobs = 2000
     ndim = 3
     persistence = .99
